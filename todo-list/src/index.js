6d549d75e11dbbf2da55d3a358e88c8b5664e5b8
Med-Ouallou <mouallou279@gmail.com>
2025-12-31 13:51:15 +0000

    booklist

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,10 +1,42 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-
-function Hello(){
-    return <h1>My first component</h1>;
+const firstBook = {
+    title : 'El Jaiz wa ghair Ljaiz fi nikah L3ajaiz',
+    author : 'mohamed ouallou',
 }
 
+const secondBook = {
+    title : 'fuck Lwatan',
+    author : 'Hokage 6',
+}
+
+
+
+function BookList(){
+    return (
+        <section>
+            <Book title={firstBook.title} auth={firstBook.author}/>
+            <Book title={secondBook.title} auth={secondBook.author}/>
+        </section>
+    );
+}
+
+// to type to get your props : 
+// 1 - const {title , author} = props;
+// 2 - const Book = ({title,author}) => {
+    
+const Book = ({title,author}) => {
+    // const {title , author} = props;
+    return(
+        <article>
+            <img src="" alt="book image"/>
+            <h2>{title}</h2>
+            <h2>{author}</h2>
+        </article>
+    )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Hello/>);
+root.render(<BookList/>);
